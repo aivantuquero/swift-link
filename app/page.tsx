8267558748d62d6b1aab84799264d2058b60d05f
Aivan Carlos Tuquero <aivancarlostuquero@gmail.com>
2024-02-02 23:11:22 +0800

    Update login button text

--- app/page.tsx
+++ app/page.tsx
@@ -94,17 +94,8 @@ export default function Home() {
                 className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
                 href="/"
               >
-                Login
+                Get Started
               </a>
-
-              <div className="hidden sm:flex">
-                <a
-                  className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
-                  href="/"
-                >
-                  Register
-                </a>
-              </div>
             </div>
 
             <div className="block md:hidden">
